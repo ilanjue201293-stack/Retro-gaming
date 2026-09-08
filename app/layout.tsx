ba1e3b7c48fc4165a067ae5868e552e6e13a4314
Nalyy <ilanjue201293@gmail.com>
2026-09-08 13:59:58 +0200

    Load Hockey V2 styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import "./hockey.css";
+import "./hockey-v2.css";
 
 export const metadata: Metadata = {
   title: "Retro Gaming",
