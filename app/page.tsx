6aa906eb49765b6ee88f92653ad11963614aab6f
Nalyy <ilanjue201293@gmail.com>
2026-09-09 08:05:18 +0200

    Mount unified bot experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import RetroApp from "./RetroApp";
 import RoomGameController from "./RoomGameController";
 import DashboardGameLibraryEnhancer from "./DashboardGameLibraryEnhancer";
+import BotUXEnhancer from "./BotUXEnhancer";
 
 export default function Home() {
   return <>
     <RetroApp />
     <RoomGameController />
     <DashboardGameLibraryEnhancer />
+    <BotUXEnhancer />
   </>;
 }
