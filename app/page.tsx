0f363fa18b2baaa947fdce4358d92592a13b253a
Nalyy <ilanjue201293@gmail.com>
2026-09-09 07:55:37 +0200

    Enhance dashboard game library

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import RetroApp from "./RetroApp";
 import RoomGameController from "./RoomGameController";
+import DashboardGameLibraryEnhancer from "./DashboardGameLibraryEnhancer";
 
 export default function Home() {
   return <>
     <RetroApp />
     <RoomGameController />
+    <DashboardGameLibraryEnhancer />
   </>;
 }
